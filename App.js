b77ab309fd2e8778d19dbb1722277b3925f6b7e6
haseeb <haseebasif630@gmail.com>
2025-05-19 14:47:11 +0500

    https://github.com/haseeb-asif09/Web-MT-.git

--- App.js
+++ App.js
@@ -4,25 +4,27 @@ import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
+import Services from './Services';
+import IndustriesServed from './IndustriesServed';
 import Careers from './Careers';
 import Blog from './Blog';
 import TermsPrivacy from './TermsPrivacy';
 import Contact from './Contact';
-import Services from './Services';
-import IndustriesServed from './IndustriesServed';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div id="home"><Home /></div>
-      <div id="about"><About /></div>
-      <div id="services"><Services /></div>         {/* Services Section */}
-      <div id="industriesserved"><IndustriesServed /></div> {/* Industries Served Section */}
-      <div id="careers"><Careers /></div>
-      <div id="blog"><Blog /></div>
-      <div id="termsprivacy"><TermsPrivacy /></div>
-      <div id="contact"><Contact /></div>
+      
+      {/* Different Sections */}
+      <section id="home"><Home /></section>
+      <section id="about"><About /></section>
+      <section id="services"><Services /></section>
+      <section id="industries-served"><IndustriesServed /></section>
+      <section id="careers"><Careers /></section>
+      <section id="blog"><Blog /></section>
+      <section id="terms-privacy"><TermsPrivacy /></section>
+      <section id="contact"><Contact /></section>
     </div>
   );
 }
